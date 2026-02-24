7424068607f67f5905d20f6b53bdcdcd645dd35e
daniel <danielbaradaran31-hash>
2026-02-24 19:07:40 +0100

    add icons

--- src/components/modules/navbar/Navbar.js
+++ src/components/modules/navbar/Navbar.js
@@ -1,22 +1,70 @@
-import React from 'react'
-import styles from './Navbar.module.css'
-const Navbar = () => {
+import React from 'react';
+import styles  from "./Navbr.module.css";
+import Link from 'next/link'
+import { IoIosArrowDown } from "react-icons/io";
+import { FaShoppingCart } from "react-icons/fa";
+
+
+function Navbar () {
   return (
     <nav className={styles.navbar}>
         <main>
             <div>
-                <link href='/'>
-                <img src="/logo.png" alt='logo' />
-                </link>
+                <Link href={'/'}>
+                <img src="/images/logo.png" alt='Logo' />
+                </Link>
             </div>
 
-            <ul className={styles.links}></ul>
+            <ul className={styles.links}>
+                <li>
+                    <Link href={'/'}>صفحه اصلی</Link>
+                </li>
+                <li>
+                    <Link href={'/category'}>فروشگاه</Link>
+                </li>
+                <li>
+                    <Link href={'/blog'}>وبلاگ</Link>
+                </li>
+                <li>
+                    <Link href={'/contact-us'}>تماس با ما</Link>
+                </li>
+                <li>
+                    <Link href={'/about-us'}>درباره ما</Link>
+                </li>
+                <li>
+                    <Link href={'/rules'}>قوانین</Link>
+                </li>
+                {/* <li>
+                    <Link href={'/login-register'}>ورود / عضویت</Link>
+                </li> */}
 
-            <div className={styles.navbar_icons}></div>
+                {/* Start My-Account Section */}
+                <div className={styles.dropdown}>
+                    <Link href={'/p-user'}>
+                    <IoIosArrowDown className={styles.dropdown_icons}/>
+
+                    حساب کاربری</Link>
+                <div className={styles.dropdown_content}>
+                    <Link href={'/p-user/orders'}>سفارشات</Link>
+                    <Link href={'/p-user/tickets'}> تیکت های پشتیبانی</Link>
+                    <Link href={'/p-user/comments'}>کامنت ها</Link>
+                    <Link href={'/p-user/wishlist'}>علاقه مندی ها</Link>
+                    <Link href={'/p-user/account-details'}>جزئیات اکانت</Link>
+                </div>
+                </div>
+                 {/* Finish My-Account Section */}
+            </ul>
+
+            <div className={styles.navbar_icons}>
+                <Link href={'/cart'}>
+                <FaShoppingCart />
+
+                </Link>
+            </div>
         </main>
 
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
