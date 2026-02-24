b995eac1e1152597367037e0ec6644dc0a80a555
daniel <danielbaradaran31-hash>
2026-02-24 21:16:19 +0100

    Add AOS

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Navbar from "@/components/modules/navbar/Navbar";
 import Banner from "@/components/templates/index/banner/Banner";
-
+import Latest from "@/components/templates/index/latest/latest";
 export default function Home() {
   return (
    <>
    <Navbar />
    <Banner />
+   <Latest />
    </>
   )
 }
