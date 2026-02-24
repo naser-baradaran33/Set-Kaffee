8c83fea99547fd7d9646ee2032436a000253d17f
daniel <danielbaradaran31-hash>
2026-02-24 22:21:41 +0100

    fix: update articles components

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Navbar from "@/components/modules/navbar/Navbar";
 import Banner from "@/components/templates/index/banner/Banner";
 import Latest from "@/components/templates/index/latest/latest";
 import Promote from "@/components/templates/index/promote/Promote";
+import Articles from "@/components/templates/index/articles/Articles";
 export default function Home() {
   return (
    <>
@@ -9,6 +10,7 @@ export default function Home() {
    <Banner />
    <Latest />
    <Promote />
+   <Articles />
    </>
   )
 }
